0d5b0bfeb5539261019d26abbc5d5feaf0c090f3
jf9860 <jf9860@att.com>
2017-02-24 14:39:40 -0500

    Guess at clone location of utils
    Change-Id: I4073963f678e2b58f9d33d6a42a4ee5eadf9246f
    Signed-off-by: jf9860 <jf9860@att.com>

--- setup.sh
+++ setup.sh
@@ -15,17 +15,18 @@ pip install --target="$path/robot/library" 'selenium<=3.0.0' 'requests==2.11.1' 
 
 
 # get the git for the eteutils you will need to add a private key to your ssh before this
-if [ -d $path/eteutils ]
+if [ -d $path/testsuite/eteutils ]
 then
-	cd $path/eteutils
-else 
+    # Support LF build location
+	cd $path/testsuite/eteutils
+else
 	cd ~
 	git config --global http.sslVerify false
 	if [ -d ~/python-testing-utils ]
 	then
 		cd python-testing-utils
 		git pull origin master
-	else 
+	else
 		git clone https://gerrit.openecomp.org/r/testsuite/python-testing-utils.git
 		cd python-testing-utils
 	fi
@@ -33,17 +34,18 @@ fi
 pip install --upgrade --target="$path/robot/library" .
 
 
-if [ -d $path/heatbridge ]
+if [ -d $path/testsuite/heatbridge ]
 then
-	cd $path/heatbridge
-else 
+    # Support LF build location
+	cd $path/testsuite/heatbridge
+else
 	cd ~
 	git config --global http.sslVerify false
 	if [ -d ~/heatbridge ]
 	then
 		cd heatbridge
 		git pull origin master
-	else 
+	else
 		git clone https://gerrit.openecomp.org/r/testsuite/heatbridge.git
 		cd heatbridge
 	fi
@@ -57,28 +59,28 @@ pip install --upgrade --target="$path/robot/library" .
 pip install --target="$path/robot/library" -U 'paramiko==2.0.2'
 
 #
-# Get the appropriate chromedriver. Default to linux64 
+# Get the appropriate chromedriver. Default to linux64
 #
 CHROMEDRIVER_URL=http://chromedriver.storage.googleapis.com/2.27
 CHROMEDRIVER_ZIP=chromedriver_linux64.zip
 
-# Handle mac and windows 
+# Handle mac and windows
 OS=`uname -s`
 case $OS in
-  MINGW*_NT*) 
+  MINGW*_NT*)
   	CHROMEDRIVER_ZIP=chromedriver_win32.zip
   	;;
   Darwin*)
-  	CHROMEDRIVER_ZIP=chromedriver_mac64.zip 
+  	CHROMEDRIVER_ZIP=chromedriver_mac64.zip
   	;;
   *) echo "Defaulting to Linux 64" ;;
 esac
 
 if [ $CHROMEDRIVER_ZIP == 'chromedriver_linux64.zip' ]
 then
-    wget -O chromedriver.zip $CHROMEDRIVER_URL/$CHROMEDRIVER_ZIP 
+    wget -O chromedriver.zip $CHROMEDRIVER_URL/$CHROMEDRIVER_ZIP
 	unzip chromedriver.zip -d /usr/local/bin
 else
-    curl $CHROMEDRIVER_URL/$CHROMEDRIVER_ZIP -o chromedriver.zip  
-	unzip chromedriver.zip 
+    curl $CHROMEDRIVER_URL/$CHROMEDRIVER_ZIP -o chromedriver.zip
+	unzip chromedriver.zip
 fi
