633a15d4c54446ace1e63a4d6c0cd9e7b1d45d71
Jerry Flood <jf9860@att.com>
2017-06-02 16:46:11 -0400

    Added support for soak test config file
    Issue: TEST-38
    Change-Id: I40fdad36019cbfa6bee6e9ef7bf6193deb27fd47
    Signed-off-by: Jerry Flood <jf9860@att.com>

--- runSoak.sh
+++ runSoak.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 INSTALL_DIR=/var/opt/OpenECOMP_ETE
-DURATION=$1
+#
+# Execute tags built to support the hands on demo,
+#
+function usage
+{
+	echo "Usage: runSoak.sh [-p <filename> -d <seconds> -c <seconds>]"
+	echo " "
+	echo "       -p, --profile"
+	echo "               - name of JSON file containing test profile"
+	echo " "
+	echo "       -d, --duration"
+	echo "               - Duration of soak test (overrides value --profile)"
+	echo " "
+	echo "       -c, --cyclelength"
+	echo "               - Time between starting iterations of profile"
+	echo "                 If longer than total run time of a single iteration,"
+	echo "                 additional wait is added before starting the next iteration."
+	echo "                 Value has no effect if it is shorter than the total run time"
+	echo "                 of a single iteration over the profile."
+	echo "                 (overrides value in --profile)"
+}
+
 
 cd ${INSTALL_DIR}
 export PYTHONPATH=${INSTALL_DIR}/robot/library
-python -m loadtest.TestMain -d ${DURATION} --logfile /share/logs/soak_$$.log
+python -m loadtest.TestMain $@ --logfile /share/logs/soak_$$.log
