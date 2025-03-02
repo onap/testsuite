554d76bb3e63c85b8261ede93040515f2138e6c6
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-03-02 16:45:25 +0100

    Update robotframework in testsuite to 3.2.2
    - declare robotframework-onap in requirements.txt and pin it's version
    - clean up deprecated syntax in Dockerfile
    - improve the README formatting and add further instructions to setup
      the project and run tests
    - add an initial robot_properties.py file that may contain variable declarations
      that are required to run the tests locally
    
    Issue-ID: INT-2295
    Change-Id: Ia913c610a6418643967ba1fd2791ae42038ac85e
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

---
+++ robot_properties.py
@@ -0,0 +1,6 @@
+# The robot tests require a global variables file that
+# contains environment specific values like service names and credentials.
+# You'll find the real values in the OOM robot chart.
+# When executed locally, these values are different and need to be adjusted
+# to the environment they are running in.
+GLOBAL_INJECTED_SO_BPMN_IP_ADDR = '127.0.0.1'
